250d1103792335c17847a75b6e87dadcb4c3177b
George Croucher <gpcroucher@gmail.com>
2024-10-11 22:18:45 +0100

    comments now show on /posts/[id]

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -3,6 +3,7 @@ import CommentForm from "@/components/CommentForm";
 import Post from "@/components/Post";
 import connect from "@/utils/db";
 import { currentUser } from "@clerk/nextjs/server";
+import { format } from "date-fns";
 import { notFound } from "next/navigation";
 
 export default async function IndividualPostPage({
@@ -26,9 +27,28 @@ export default async function IndividualPostPage({
   return (
     <>
       <Post post={post} />
+      {(await getComments()).map(
+        ({ comment_id, body, created_at, clerk_id }) => {
+          return (
+            <div key={comment_id} className="bg-slate-500">
+              <p>{body}</p>
+              <p>{format(created_at, "E do MMM y, kk:mm:ss")}</p>
+              <p>{clerk_id}</p>
+            </div>
+          );
+        },
+      )}
       <CommentForm post_id={post_id} username={username} />
     </>
   );
+
+  async function getComments() {
+    const dbResult = await db.query(
+      `SELECT * FROM week09_comments WHERE post_id = $1`,
+      [post_id],
+    );
+    return await dbResult.rows;
+  }
 }
 
 export async function serverSendComment(formData: FormData, post_id: string) {
