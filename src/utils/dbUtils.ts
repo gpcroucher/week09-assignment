30617e3a7c04c9dbf60c4e69bec903d284c9a3dc
George Croucher <gpcroucher@gmail.com>
2024-10-12 14:55:19 +0100

    getCurrentUser() utility

--- src/utils/dbUtils.ts
+++ src/utils/dbUtils.ts
@@ -20,6 +20,12 @@ export async function checkAndSubmitUser() {
   return dbResult.rows[0].user_id as number;
 }
 
+export async function getCurrentUser() {
+  const user = await currentUser(); // todo: save this to localstorage to avoid pinging clerk so often
+  const clerk_id = user?.id as string;
+  return await getUserByClerkId(clerk_id);
+}
+
 export async function getUserByClerkId(clerk_id: string) {
   const dbResult = await db.query(
     `SELECT * FROM week09_users WHERE clerk_id = $1`,
