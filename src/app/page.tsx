960c032a439e81623603ccbdbd38831a70a9bea9
George Croucher <gpcroucher@gmail.com>
2024-10-13 21:08:31 +0100

    slightly improved homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to CommentForm</h1>
+      <h1 className="text-center text-4xl">Welcome to CommentForm&trade;</h1>
+      <br />
+      <p className="text-center">
+        Here you can post about your projects and celebrate each other&apos;s
+        achievements!
+      </p>
     </div>
   );
 }
