7020622a04dea5c7c33f1ba7ef27cb76f1b576d4
George Croucher <gpcroucher@gmail.com>
2024-10-13 21:48:46 +0100

    page now rerenders when you post a comment

--- src/utils/serverSendComment.ts
+++ src/utils/serverSendComment.ts
@@ -1,6 +1,8 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import connect from "./db";
 import { checkAndSubmitUser } from "./dbUtils";
+import { redirect } from "next/navigation";
 
 export async function serverSendComment(formData: FormData, post_id: string) {
   const body = formData.get("body") as string;
@@ -12,4 +14,7 @@ export async function serverSendComment(formData: FormData, post_id: string) {
     `INSERT INTO week09_comments (body, post_id, user_id) VALUES ($1, $2, $3)`,
     [body, post_id, user_id],
   );
+
+  revalidatePath(`/posts/${post_id}`);
+  redirect(`/posts/${post_id}`);
 }
