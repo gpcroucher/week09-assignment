d913227b14fabf8823b659774d0e92931ac003ff
George Croucher <gpcroucher@gmail.com>
2024-10-13 21:24:32 +0100

    NewPostForm component styling

--- src/components/NewPostForm.tsx
+++ src/components/NewPostForm.tsx
@@ -17,20 +17,47 @@ export default function NewPostForm({ serverAction }: NewPostFormProps) {
   }
 
   return (
-    <form onSubmit={handleSubmit} className="border border-black bg-slate-400">
+    <form
+      onSubmit={handleSubmit}
+      className="rounded-xl border border-black bg-slate-100 p-2"
+    >
       <label htmlFor="titleInput">Title</label>
-      <input type="text" name="title" id="titleInput" />
+      <input
+        type="text"
+        name="title"
+        id="titleInput"
+        className="m-1 rounded-md border border-slate-600"
+      />
       <br />
-      <label htmlFor="bodyInput">Your post</label>
-      <textarea name="body" id="bodyInput" />
+      <div className="flex items-center">
+        <label htmlFor="bodyInput">Your post</label>
+        <textarea
+          name="body"
+          id="bodyInput"
+          className="m-1 rounded-md border border-slate-600"
+        />
+      </div>
+
       <p>
         Optionally, add a link to your deployment and/or your source repository.
       </p>
       <label htmlFor="liveUrlInput">Deployment URL</label>
-      <input type="text" name="live_url" id="liveUrlInput" />
+      <input
+        type="text"
+        name="live_url"
+        id="liveUrlInput"
+        className="m-1 rounded-md border border-slate-600"
+      />
       <label htmlFor="titleInput">Repository URL</label>
-      <input type="text" name="repo_url" id="repoUrlInput" />
-      <button>Submit Post</button>
+      <input
+        type="text"
+        name="repo_url"
+        id="repoUrlInput"
+        className="m-1 rounded-md border border-slate-600"
+      />
+      <button className="m-1 rounded-md border border-slate-600 p-1">
+        Submit Post
+      </button>
     </form>
   );
 }
